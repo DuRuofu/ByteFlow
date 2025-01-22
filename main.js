fa3b3db42869e60bbc144d1b0ad289f816a88ba0
DuRuofu <duruofu@qq.com>
2025-01-22 19:41:47 +0800

    update: 原始开发模式

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow, ipcMain } = require('electron/main')
+const { app, BrowserWindow, ipcMain,Menu  } = require('electron/main')
 const path = require('node:path')
 const fs = require('fs')
 
@@ -35,6 +35,11 @@ const createWindow = () => {
   ipcMain.handle('read-file', readFile)
 
   win.loadFile('./pages/index.html')
+
+  //定义菜单
+  const mainMenu = Menu.buildFromTemplate(  menuTemplate  );
+  //设置菜单，才展示出来
+  Menu.setApplicationMenu( mainMenu );
 }
 
 app.whenReady().then(() => {
@@ -52,3 +57,55 @@ app.on('window-all-closed', () => {
     app.quit()
   }
 })
+
+//定义菜单
+const menuTemplate = [
+    {
+        label:'文件',
+        submenu:[
+            {
+                label:'新增一条记录',
+                click:()=>{
+                    createAddWindow();
+                }
+            },
+            {
+                label:'退出程序',
+                accelerator:process.platform=='darwin'?'Command + W' : 'ctrl + W',
+                click:()=>{
+                    app.quit();
+                }
+            }
+        ]
+    },
+    {
+        label:'开发者工具',
+        submenu:[
+            {
+                label:'打开/关闭',
+                //设定快捷键
+                accelerator:process.platform=='darwin'?'Command + I' : 'ctrl + I',
+                //菜单事件
+                click:( item , focusedWindow )=>{
+                    focusedWindow.toggleDevTools();
+                }
+            },
+            {
+                label:'刷新页面',
+                role:'reload',
+                accelerator:process.platform=='darwin'?'Command + T' : 'ctrl + T',
+            }
+        ]
+    }
+];
+
+const createAddWindow = ()=>{
+    
+    const wins = new BrowserWindow({
+        width: 800,
+        height: 600
+    })
+    
+    wins.loadFile('./pages/add.html');
+
+}
