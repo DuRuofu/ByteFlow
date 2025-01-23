7daecc1f0f6fe27a8cecfb9e4eae55b16f18b36d
DuRuofu <duruofu@qq.com>
2025-01-23 11:28:55 +0800

    feat:优化页面

--- src/renderer/src/App.vue
+++ src/renderer/src/App.vue
@@ -1,113 +1,118 @@
 <template>
-  <el-tabs v-model="activeName" class="demo-tabs" @tab-click="handleClick">
-    <el-tab-pane label="串口" name="first">
-      <template #label>
-        <span class="custom-tabs-label">
-          <el-icon>
-            <calendar />
-          </el-icon>
-          <span>串口</span>
-        </span>
-      </template>
-      <div class="common-layout">
-        <el-container>
-          <el-aside class="Aside">
-            <el-form :model="form" label-width="auto" style="max-width: 600px">
-              <el-form-item label="串口名:" label-position="left">
-                <el-select v-model="form.SerialPort" placeholder="">
-                  <el-option label="Zone one" value="shanghai" />
-                  <el-option label="Zone two" value="beijing" />
-                </el-select>
-              </el-form-item>
-              <el-form-item label="波特率:" label-position="left">
-                <el-select v-model="form.SerialPort" placeholder="">
-                  <el-option label="Zone one" value="shanghai" />
-                  <el-option label="Zone two" value="beijing" />
-                </el-select>
-              </el-form-item>
-              <el-form-item label="数据位:" label-position="left">
-                <el-select v-model="form.SerialPort" placeholder="">
-                  <el-option label="Zone one" value="shanghai" />
-                  <el-option label="Zone two" value="beijing" />
-                </el-select>
-              </el-form-item>
-              <el-form-item label="校验位:" label-position="left">
-                <el-select v-model="form.SerialPort" placeholder="">
-                  <el-option label="Zone one" value="shanghai" />
-                  <el-option label="Zone two" value="beijing" />
-                </el-select>
-              </el-form-item>
-              <el-form-item label="停止位:" label-position="left">
-                <el-select v-model="form.SerialPort" placeholder="">
-                  <el-option label="Zone one" value="shanghai" />
-                  <el-option label="Zone two" value="beijing" />
-                </el-select>
-              </el-form-item>
-              <el-form-item>
-                <el-button type="primary" style="width: 90%" @click="onSubmit">连接</el-button>
-              </el-form-item>
-              <el-form-item label="接收设置" label-position="left">
-                <el-checkbox-group v-model="form.type">
-                  <el-checkbox value="Online activities" name="type">
-                    Online activities
-                  </el-checkbox>
-                  <el-checkbox value="Promotion activities" name="type">
-                    Promotion activities
-                  </el-checkbox>
-                  <el-checkbox value="Offline activities" name="type">
-                    Offline activities
-                  </el-checkbox>
-                  <el-checkbox value="Simple brand exposure" name="type">
-                    Simple brand exposure
-                  </el-checkbox>
-                </el-checkbox-group>
-              </el-form-item>
-              <el-form-item label="发送设置" label-position="left">
-                <el-checkbox-group v-model="form.type">
-                  <el-checkbox value="Online activities" name="type">
-                    Online activities
-                  </el-checkbox>
-                  <el-checkbox value="Promotion activities" name="type">
-                    Promotion activities
-                  </el-checkbox>
-                </el-checkbox-group>
-              </el-form-item>
-              <el-form-item label="其他" label-position="left">
-                <el-radio-group v-model="form.resource">
-                  <el-radio value="Sponsor">Sponsor</el-radio>
-                  <el-radio value="Venue">Venue</el-radio>
-                </el-radio-group>
-              </el-form-item>
-              <el-form-item>
-                <el-button type="primary" @click="onSubmit">Create</el-button>
-                <el-button>Cancel</el-button>
-              </el-form-item>
-            </el-form>
-          </el-aside>
-          <el-main class="Main">
-            <div class="ReceiveBuffer">
-              <el-input v-model="textarea" type="textarea" placeholder="" resize="none" />
-            </div>
+  <div style='position: relative;'>
 
-            <div class="SendBuffer">
 
-              <el-input v-model="textarea" type="textarea" placeholder="" resize="none" />
-              <el-button>发送</el-button>
-            </div>
-            <div class="ReceiveBufferInfo">
-              <div>接收数据：xx字节</div>
-              <div>发送数据：xx字节</div>
-              <div>重新计数</div>
-            </div>
-          </el-main>
-        </el-container>
-      </div>
-    </el-tab-pane>
-    <el-tab-pane label="示波器" name="second">Config</el-tab-pane>
-    <el-tab-pane label="数据引擎" name="third">Role</el-tab-pane>
-    <el-tab-pane label="设置" name="fourth">Task</el-tab-pane>
-    <el-tab-pane label="关于" name="fifth">Task</el-tab-pane>
-  </el-tabs>
+    <el-tabs v-model="activeName" class="tabs" @tab-click="handleClick">
+      <el-tab-pane label="串口" name="first">
+        <template #label>
+          <span class="tabs-label">
+            <el-icon>
+              <calendar />
+            </el-icon>
+            <span>串口</span>
+          </span>
+        </template>
+        <div class="layout-serial">
+          <el-container>
+            <el-aside class="serial-aside">
+              <el-form :model="form" label-width="auto" style="max-width: 600px">
+                <el-form-item label="串口名:" label-position="left">
+                  <el-select v-model="form.SerialPort" placeholder="">
+                    <el-option label="Zone one" value="shanghai" />
+                    <el-option label="Zone two" value="beijing" />
+                  </el-select>
+                </el-form-item>
+                <el-form-item label="波特率:" label-position="left">
+                  <el-select v-model="form.SerialPort" placeholder="">
+                    <el-option label="Zone one" value="shanghai" />
+                    <el-option label="Zone two" value="beijing" />
+                  </el-select>
+                </el-form-item>
+                <el-form-item label="数据位:" label-position="left">
+                  <el-select v-model="form.SerialPort" placeholder="">
+                    <el-option label="Zone one" value="shanghai" />
+                    <el-option label="Zone two" value="beijing" />
+                  </el-select>
+                </el-form-item>
+                <el-form-item label="校验位:" label-position="left">
+                  <el-select v-model="form.SerialPort" placeholder="">
+                    <el-option label="Zone one" value="shanghai" />
+                    <el-option label="Zone two" value="beijing" />
+                  </el-select>
+                </el-form-item>
+                <el-form-item label="停止位:" label-position="left">
+                  <el-select v-model="form.SerialPort" placeholder="">
+                    <el-option label="Zone one" value="shanghai" />
+                    <el-option label="Zone two" value="beijing" />
+                  </el-select>
+                </el-form-item>
+                <el-form-item>
+                  <el-button type="primary" style="width: 90%" @click="onSubmit">连接</el-button>
+                </el-form-item>
+                <el-form-item label="接收设置" label-position="left">
+                  <el-checkbox-group v-model="form.type">
+                    <el-checkbox value="Online activities" name="type">
+                      Online activities
+                    </el-checkbox>
+                    <el-checkbox value="Promotion activities" name="type">
+                      Promotion activities
+                    </el-checkbox>
+                    <el-checkbox value="Offline activities" name="type">
+                      Offline activities
+                    </el-checkbox>
+                    <el-checkbox value="Simple brand exposure" name="type">
+                      Simple brand exposure
+                    </el-checkbox>
+                  </el-checkbox-group>
+                </el-form-item>
+                <el-form-item label="发送设置" label-position="left">
+                  <el-checkbox-group v-model="form.type">
+                    <el-checkbox value="Online activities" name="type">
+                      Online activities
+                    </el-checkbox>
+                    <el-checkbox value="Promotion activities" name="type">
+                      Promotion activities
+                    </el-checkbox>
+                  </el-checkbox-group>
+                </el-form-item>
+                <el-form-item label="其他" label-position="left">
+                  <el-radio-group v-model="form.resource">
+                    <el-radio value="Sponsor">Sponsor</el-radio>
+                    <el-radio value="Venue">Venue</el-radio>
+                  </el-radio-group>
+                </el-form-item>
+                <el-form-item>
+                  <el-button type="primary" @click="onSubmit">Create</el-button>
+                  <el-button>Cancel</el-button>
+                </el-form-item>
+              </el-form>
+            </el-aside>
+            <el-main class="serial-main">
+              <div class="serial-receive-text-box">
+                <el-input v-model="textarea" type="textarea" placeholder="" resize="none" />
+              </div>
+
+              <div class="serial-send-text-box">
+
+                <el-input v-model="textarea" type="textarea" placeholder="" resize="none" />
+                <el-button>发送</el-button>
+              </div>
+              <div class="serial-receive-info-box">
+                <div>接收数据：xx字节</div>
+                <div>发送数据：xx字节</div>
+                <div>重新计数</div>
+              </div>
+            </el-main>
+          </el-container>
+        </div>
+      </el-tab-pane>
+      <el-tab-pane label="示波器" name="second">Config</el-tab-pane>
+      <el-tab-pane label="数据引擎" name="third">Role</el-tab-pane>
+      <el-tab-pane label="设置" name="fourth">Task</el-tab-pane>
+      <el-tab-pane label="关于" name="fifth">Task</el-tab-pane>
+    </el-tabs>
+    <el-button class="button-seting" size='mini'>设置</el-button>
+  </div>
 </template>
 <script lang="ts" setup>
 import { ref, reactive } from 'vue'
@@ -125,7 +130,7 @@ const form = reactive({
   delivery: false,
   type: [],
   resource: '',
-  desc: '',
+  desc: ''
 })
 const textarea = ref('')
 const onSubmit = () => {
@@ -138,20 +143,30 @@ const handleClick = (tab: TabsPaneContext, event: Event) => {
 </script>
 
 <style scoped lang="scss">
-.demo-tabs .el-tabs__content {
+.tabs .el-tabs__content {
   padding: 32px;
   color: #6b778c;
   font-size: 32px;
   font-weight: 600;
 }
 
-.common-layout {
-  height: calc(100vh - 70px);
+.tabs .tabs-label .el-icon {
+  vertical-align: middle;
+}
+
+.tabs .tabs-label span {
+  vertical-align: middle;
+  margin-left: 4px;
+}
+
+.layout-serial {
+
+  height: calc(100vh - 73px);
   /* 减去页头的高度 */
   width: 100%;
 
-  .Aside {
-    height: calc(100vh - 70px);
+  .serial-aside {
+    height: calc(100vh - 73px);
     /* 高度继承父元素 */
     //width: 30%;
     // 最大宽度
@@ -160,8 +175,8 @@ const handleClick = (tab: TabsPaneContext, event: Event) => {
     margin-right: 10px;
   }
 
-  .Main {
-    height: calc(100vh - 70px);
+  .serial-main {
+    height: calc(100vh - 73px);
     /* 高度继承父元素 */
     //width: 70%;
     padding: 0;
@@ -172,10 +187,9 @@ const handleClick = (tab: TabsPaneContext, event: Event) => {
     /* 子元素纵向排列 */
     overflow: hidden;
 
-    .ReceiveBuffer {
+    .serial-receive-text-box {
       height: 87%;
       width: 100%;
-      background-color: #631b1b;
 
       .el-input,
       .el-textarea {
@@ -186,14 +200,13 @@ const handleClick = (tab: TabsPaneContext, event: Event) => {
       }
     }
 
-    .SendBuffer {
+    .serial-send-text-box {
       display: flex;
       /* 启用 Flexbox 布局 */
       align-items: center;
       /* 垂直方向居中 */
       height: 10%;
       width: 100%;
-      background-color: #491c1c;
       margin-top: 10px;
 
       .el-input {
@@ -220,7 +233,7 @@ const handleClick = (tab: TabsPaneContext, event: Event) => {
       }
     }
 
-    .ReceiveBufferInfo {
+    .serial-receive-info-box {
       display: flex;
       /* 启用 Flexbox 布局 */
       justify-content: space-between;
@@ -255,5 +268,11 @@ const handleClick = (tab: TabsPaneContext, event: Event) => {
       /* 深度样式处理 */
     }
   }
+}
+
+.button-seting {
+  position: absolute;
+  right: 10px;
+  top: 5px;
 }
 </style>
